016d0866935bfd5356ac132183acb031f62d1800
Asigan <Zacharycgrippo@gmail.com>
2021-09-13 15:24:48 -0500

    enemy can now attack

--- Game.cs
+++ Game.cs
@@ -251,6 +251,7 @@ namespace BattleArena
                     if (input == 1)
                     {
                         Attack(ref player, ref currentEnemy);
+                        Attack(ref currentEnemy, ref player);
                     }
                     if (input == 2)
                     {
